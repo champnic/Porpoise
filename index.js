3533ab3e9246015eaa6ad32ca7680b2de13d47ab
Nic Champagne Williamson <champnic@microsoft.com>
2022-03-21 17:39:09 -0700

    Finally able to get a workitem :)

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ require("dotenv").config();
 // Uncomment this line to verify that the environment and PATs are loaded correctly
 //console.log(process.env);
 
-let edgeAdoUrl = "https://microsoft.visualstudio.com/Edge";
+let edgeAdoUrl = "https://dev.azure.com/microsoft";
+let edgeProject = "Edge";
 let edgeAdoPat = process.env.EDGE_ADO_PAT;
 
 let authHandler = ado.getPersonalAccessTokenHandler(edgeAdoPat);
 let edgeAdo = new ado.WebApi(edgeAdoUrl, authHandler);
+
+async function run() {
+    let adoWork = await edgeAdo.getWorkItemTrackingApi();
+    let myBug = await adoWork.getWorkItem(38617678);
+}
+
+run();
