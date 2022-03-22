1c814efdb9d648c82305dbe5a55f39e62c420f9e
Nic Champagne Williamson <champnic@microsoft.com>
2022-03-22 11:51:47 -0700

    Pretty close to having the metrics ADO description update working.

--- index.js
+++ index.js
@@ -10,9 +10,16 @@ let edgeAdoPat = process.env.EDGE_ADO_PAT;
 
 let authHandler = ado.getPersonalAccessTokenHandler(edgeAdoPat);
 let edgeAdo = new ado.WebApi(edgeAdoUrl, authHandler);
+//let adoWork = {};
 
 async function run() {
+    // Initialize connection to ADO work tracking.
+    let adoWork = await edgeAdo.getWorkItemTrackingApi();
+}
 
+// Set the result of the calculation into custom string 3.
+async function saveCalculation(adoId, result) {
+    //adoWork.
 }
 
 // This function is just a testbed for how the ADO API works, like what values are
@@ -21,12 +28,49 @@ async function investigateAdoInfo() {
     let testId = 38617678;
 
     let adoWork = await edgeAdo.getWorkItemTrackingApi();
-    let myBug = await adoWork.getWorkItem(38617678);
-    //adoWork.createField()
-    let bugType = await adoWork.getWorkItemType("Edge", "Bug");
-    let scenarioType = await adoWork.getWorkItemType("Edge", "Scenario");
+    
+    // Get work item
+    let myBug = await adoWork.getWorkItem(testId);
+    let descriptionFieldName = myBug.fields["Microsoft.VSTS.CMMI.TaskType"] == "Bug"
+        ? "Microsoft.VSTS.TCM.ReproSteps"
+        : "System.Description";
+    let currentDescription = myBug.fields[descriptionFieldName] ?? "";
 
-    let fields = await adoWork.getFields("Edge");
+    let startTag = "<br>---- GitHub Metrics (auto-generated) ----<br>"
+    let endTag = "<br>---- End GitHub Metrics ----<br>"
+    let startIndex = currentDescription.indexOf(startTag);
+    let endIndex = currentDescription.indexOf(endTag);
+
+    let startString =  currentDescription;
+    let endString = "";
+    if (startIndex >= 0 && endIndex >= 0) {
+        startString = currentDescription.substring(0, startIndex);
+        endString = currentDescription.substring(endIndex + endTag.length);
+    }
+
+    let metricsString = "Likes: 10, Users: 12, Result: 101";
+    let newDescription = startString + startTag + metricsString + endTag + endString;
+
+    // Work item types
+    //let bugType = await adoWork.getWorkItemType("Edge", "Bug");
+    //let scenarioType = await adoWork.getWorkItemType("Edge", "Scenario");
+
+    // All fields
+    //let fields = await adoWork.getFields("Edge");
+
+    // The "patchDoc" describes what fields of the workitem should be updated, and the values.
+    let patchDoc = [];
+    patchDoc.push({
+        op: "add",
+        path: "/fields/Microsoft.VSTS.Common.CustomString03",
+        value: 100
+    });
+    patchDoc.push({
+        op: "add",
+        path: "/fields/" + descriptionFieldName,
+        value: newDescription
+    });
+    await adoWork.updateWorkItem([], patchDoc, 38617678);
 }
 
-run();
+investigateAdoInfo();
